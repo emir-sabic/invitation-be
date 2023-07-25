13546b52e8ccdc9b78e870f43c4853a0f4488452
emir-sabic <sabice85@gmail.com>
2023-07-25 14:53:51 +0200

    first

---
+++ src/main/java/com/invitationbe/InvitationBeApplication.java
@@ -0,0 +1,13 @@
+package com.invitationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvitationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvitationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invitationbe/InvitationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invitationbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvitationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
